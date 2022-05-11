0ee928d86ec10e839382d1580a9e78e7db794cbf
justingracie <j.m.gracie@gmail.com>
2022-05-11 14:21:17 -0600

    added alts to images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -20,7 +20,7 @@ useEffect(() => {
         return projects.map((project, idx) => (
             <div key={idx}>
                 <h1>{project.name}</h1>
-                <img src={project.image} />
+                <img src={project.image} alt={project.name} />
                 <a href={project.git}>
                     <button>Github</button>
                 </a>
